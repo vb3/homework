e9ba8bb2429e5032ee5cfdc44c30f757df0af2b2
Vinay Bikkina <vb2303@gmail.com>
2013-02-13 11:25:21 -0800

    works for order 3, not for order 4. Added function from online site for testing...

--- CS325H/HW5/HW5/main.c
+++ CS325H/HW5/HW5/main.c
@@ -13,26 +13,28 @@
 
 int *array_op(int *A, int *B, int len, int add);
 int *poly_multi_divcon(int *P, int *Q, int n);
+int *polyMult(int *p, int *q, int n);
 
 int main(int argc, const char * argv[])
 {
     int *a, *b, *c = NULL;
     int i;
-    int order = 3; //atoi(argv[1]) + 1;
+    int order = atoi(argv[1]) + 1;
     
     a = (int *) calloc(order, sizeof(int));
     b = (int *) calloc(order, sizeof(int));
     
     //User Input
     printf("Enter the coefficients of A with powers in ascending order:\n");
-    for (i = 0; i < order+1; i++)
+    for (i = 0; i < order; i++)
         scanf("%d", &a[i]);
     printf("Enter the coefficients of B with powers in ascending order:\n");
-    for (i = 0; i < order+1; i++)
+    for (i = 0; i < order; i++)
         scanf("%d", &b[i]);
     
     //Compute
-    c = poly_multi_divcon(a, b, order+1);
+    //c = polyMult(a, b, order+1);
+    c = poly_multi_divcon(a, b, order);
     
     //Print results
     for (i = 2*(order+1); i >= 0; i--) {
@@ -57,14 +59,14 @@ int *poly_multi_divcon(int *P, int *Q, int n)
     int d = n/2 + n%2;
     int dd = d*2;
 
-    int *PP = array_op(P, &P[d], d, 1);      //add P1 and P2
-    int *QQ = array_op(Q, &Q[d], d, 1);      //add Q1 and Q2
+    int *PP = array_op(P, &P[d], d-n%2, d);      //add P1 and P2
+    int *QQ = array_op(Q, &Q[d], d-n%2, d);      //add Q1 and Q2
     
     int *R = poly_multi_divcon(P, Q, d);        //Mult P1*Q1
     int *S = poly_multi_divcon(PP, QQ, d);      //Mult (P1+P2)(Q1+Q2)
     int *T = poly_multi_divcon(&P[d], &Q[d], d);//Mult P2*Q2
     
-    int *outt = calloc((n*2)+1, sizeof(int));
+    int *outt = calloc(((n+1)*2), sizeof(int));
     
     //x^2d(R) + x^d(S-R-T) + T
     for (i = 0; i<=(d==1?0:d); i++) { //if d=1 then only need to do loop once, otherwise we get artifacts 
@@ -77,15 +79,58 @@ int *poly_multi_divcon(int *P, int *Q, int n)
     return outt;
 }
 
-/* Adds or Subs A and B. add=1 for add, add=0 for sub */
-int *array_op(int *A, int *B, int len, int add)
+int *array_op(int *A, int *B, int lenA, int lenB)
 {
     int i;
-    int *out = calloc(len, sizeof(int));
+    int lenC = (lenA<=lenB?lenA:lenB);
+    int *out = calloc(lenC, sizeof(int));
     
-    for (i=0; i<len; i++)
-        if (add)    out[i] = A[i] + B[i];
-        else        out[i] = A[i] - B[i];
+    for (i=0; i<lenC; i++)
+        out[i] = A[i] + B[i];
+    
+    if (lenA>lenB)
+        for (i=lenB; i<lenA; i++)
+            out[i] = A[i];
+    
+    if (lenA<lenB)
+        for (i=lenA; i<lenB; i++)
+            out[i] = B[i];
     
     return out;
+}
+
+// http://www.cs.vassar.edu/~cs241/teneyck/divideandconquer.pdf
+int *polyMult(int *p, int *q, int n) {
+    if (n == 1) {
+        int *out = malloc(sizeof(int));
+        *out = p[0] * q[0];
+        return out;
+    }
+    
+    int d = n/2 + n%2;
+    //split p and q
+    int p2[d];
+    int q2[d];
+    for (int i = 0; i < d; i++) {
+        p2[i] = p[i];
+        q2[i] = q[i];
+    }
+    int p1[d-n%2];
+    int q1[d-n%2];
+    for (int i = 0; i < d; i++) {
+        p1[i] = p[i+d];
+        q1[i] = q[i+d];
+    }
+    int *r = polyMult(p2,q2,d);
+    int *s = polyMult(array_op(p2, p1, d, d-n%2), array_op(p2, p1, d, d-n%2), d);
+    int *t = polyMult(p1,q1,d);
+    int *prod = calloc(2*n, sizeof(int));
+
+    //x^2d(R) + x^d(S-R-T) + T
+    for (int i = 0; i < d; i++) {
+        prod[i] = r[i];
+        prod[i+d] += s[i] - r[i] - t[i];
+        prod[i+2*d] += t[i];
+    }
+    return prod;
 }
