b7306efaa70a8ba61a912cfc4650270bf7742cb2
Vinay Bikkina <vb2303@gmail.com>
2013-04-14 16:17:52 -0700

    initial

---
+++ CS331/Assignment1/Assignment1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Vinay Bikkina on 4/14/13.
+//  Copyright (c) 2013 Vinay Bikkina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
